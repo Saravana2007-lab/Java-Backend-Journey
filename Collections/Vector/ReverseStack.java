d9ef1d269ac2521dfa15d776ed25457290eb0331
Saravana G <saravanag032@gmail.com>
2026-07-27 20:37:24 +0530

    feat: add ReverseStack and stack classes for string reversal and stack operations

---
+++ Collections/Vector/ReverseStack.java
@@ -0,0 +1,21 @@
+package Collections.Vector;
+import java.util.Scanner;
+import java.util.Stack;
+public class ReverseStack {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        Stack<Character> st = new Stack<>();
+
+          for(int i = 0; i < str.length(); i++){
+            st.push(str.charAt(i));
+        }
+
+           System.out.print("Reversed String: ");
+        while(!st.isEmpty()){
+            System.out.print(st.pop());
+        }
+    }
+}
---
+++ Collections/Vector/stack.java
@@ -0,0 +1,19 @@
+package Collections.Vector;
+import java.util.Stack;
+public class stack {
+	public static void main(String[] args) {
+		Stack<String> st = new Stack<>();
+        st.push("Java");
+         st.push("C+");
+          st.push("python");
+          for(String s : st){
+            System.out.println(s);
+          }
+          System.out.println("--------------------");
+          System.out.println("the removed was :" + st.pop());
+           System.out.println("--------------------");
+             for(String s : st){
+            System.out.println(s);
+          }
+	}
+}
