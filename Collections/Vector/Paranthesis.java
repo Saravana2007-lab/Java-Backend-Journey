9ec693dace1c4fdd1b2c8e7a9e667ba777ea767e
Saravana G <saravanag032@gmail.com>
2026-07-30 19:35:02 +0530

    refactor: rename Braket class to Bracket and improve balance checking logic

--- Collections/Vector/Paranthesis.java
+++ Collections/Vector/Paranthesis.java
@@ -1,22 +1,34 @@
 package Collections.Vector;
 import java.util.Stack;
 
-class braket{
-    Stack<String> b = new Stack<>();
-void check(String exp){
-for (int i = 0; i < exp.length(); i++) {
+class Bracket {
+    Stack<Character> stack = new Stack<>();
 
-    char ch = exp.charAt(i);
-if (ch == '(') {
-
-    b.push(ch);
-
-}
-}
+    void check(String expression) {
+        for (char ch : expression.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    System.out.println("Not Balanced");
+                    return;
+                }
+                char top = stack.pop();
+                if (!matches(top, ch)) {
+                    System.out.println("Not Balanced");
+                    return;
+                }
+            }
+        }
+   
     }
+
 }
+
 public class Paranthesis {
     public static void main(String[] args) {
-        
+        Bracket bracketChecker = new Bracket();
+
+        bracketChecker.check("{[()]}");   
     }
 }
