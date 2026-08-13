6a36e58f3ecfd1688eb9f3a8777a37e4779fff4f
Saravana G <saravanag032@gmail.com>
2026-08-13 19:10:36 +0530

    feat: enhance Student class and comparators for sorting by average marks, name, and ID

--- Collections/Set/StudentComparatorDemo.java
+++ Collections/Set/StudentComparatorDemo.java
@@ -1,10 +1,14 @@
 package Collections.Set;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Student{
     private String id;
     private String name;
-    private double marks;
+    private double[] marks;
 
-    Student(String id, String name, double marks) {
+    Student(String id, String name, double[] marks) {
         this.id = id;
         this.name = name;
         this.marks = marks;
@@ -27,36 +31,99 @@ class Student{
         return name;
     }
 
-    double getMarks() {
+    double[] getMarks() {
         return marks;
     }
 
     @Override
     public String toString() {
-        return "Student{id='" + id + "', name='" + name + "', marks=" + marks + '}';
+        return "Student{id='" + id + "', name='" + name + "', marks=" + java.util.Arrays.toString(marks) + '}';
     }
     
 
 }
 
-class MarksComparator{
-      @Override
-public int compareTo(Student s) {
-    int avgCompare = Double.compare(s.getAverage(), this.getAverage());
+class MarksComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        int avgCompare = Double.compare(s2.getAverage(), s1.getAverage());
         if (avgCompare == 0) {
-        return Integer.compare(this.id, s.id);
+            return s1.getId().compareTo(s2.getId());
+        }
+        return avgCompare;
     }
-    return avgCompare;
-}
 
 }
-class NameComparator{
-
+class NameComparator implements Comparator<Student>{
+    @Override
+    public int compare(Student s1, Student s2) {
+        int nameCompare = s1.getName().compareTo(s2.getName());
+        if (nameCompare == 0) {
+            return s1.getId().compareTo(s2.getId());
+        }
+        return nameCompare;
+    }
 }
-class IdComparator{
-
+class IdComparator implements Comparator<Student>{
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.getId().compareTo(s2.getId());
+    }
 }
 
 public class StudentComparatorDemo {
-    
+     public static void main(String[] args) {
+
+        Student s1 = new Student(
+                "S103",
+                "Rahul",
+                new double[]{80, 90, 70}
+        );
+
+        Student s2 = new Student(
+                "S101",
+                "Amit",
+                new double[]{95, 85, 90}
+        );
+
+        Student s3 = new Student(
+                "S102",
+                "Priya",
+                new double[]{75, 80, 85}
+        );
+
+        Student s4 = new Student(
+                "S104",
+                "Amit",
+                new double[]{90, 90, 90}
+        );
+
+        Student[] students = {s1, s2, s3, s4};
+
+        System.out.println("Original order:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+
+        Arrays.sort(students, new MarksComparator());
+
+        System.out.println("\nSorted by Average Marks:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+
+        Arrays.sort(students, new NameComparator());
+
+        System.out.println("\nSorted by Name:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+
+        Arrays.sort(students, new IdComparator());
+
+        System.out.println("\nSorted by ID:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
 }
