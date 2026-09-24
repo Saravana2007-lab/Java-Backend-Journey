754e3a27f9dd4e0bbe312c74a4240861ff14bb42
Saravana G <saravanag032@gmail.com>
2026-09-24 21:17:33 +0530

    refactor: rename ArrayListLinkedListBeginner class to ArrayListLinkedList

--- revision/ArrayListLinkedList.java
+++ revision/ArrayListLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ArrayListLinkedListBeginner {
+public class ArrayListLinkedList{
     public static void main(String[] args) {
         ArrayList<String> students = new ArrayList<>();
         students.add("Alice");
